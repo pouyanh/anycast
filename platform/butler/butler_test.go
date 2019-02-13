81f5df52aa9e946107293086cd457e25b9eae7e0
Pouyan Heyratpour <pouyan@janstun.com>
2019-02-14 00:21:32 +0330

    Fix Butler Test Syntax Errors
    Signed-off-by: Pouyan Heyratpour <pouyan@janstun.com>

--- platform/butler/butler_test.go
+++ platform/butler/butler_test.go
@@ -3,12 +3,15 @@ package butler
 import (
 	"testing"
 
-	"github.com/pouyanh/anycast/lib/infrastructure"
+	"github.com/pouyanh/anycast/platform/prosecution"
+	"github.com/pouyanh/anycast/platform/services/logger"
+	"github.com/pouyanh/anycast/platform/services/repository"
 )
 
 func TestButler_RequestForHelp(t *testing.T) {
-	logger := port.MockLevelledLogger{}
+	mll := logger.MockLevelledLogger{}
+	servants := make(repository.InMemoryServantRepository, 0)
 
-	app := NewButler(&logger)
-	app.RequestForHelp()
+	app := NewButler(&mll, servants)
+	app.RequestForHelp(prosecution.Petition{})
 }
